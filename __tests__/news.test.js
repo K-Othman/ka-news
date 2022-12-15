59bf3f32ad1da5b829f28bf062af850e63988418
K-Othman <karim_othman88@hotmail.com>
2022-12-15 14:20:51 +0000

    completed task 6

--- __tests__/news.test.js
+++ __tests__/news.test.js
@@ -1,16 +1,17 @@
 const request = require("supertest");
 const app = require("../app");
-const db = require("../db");
+const db = require("../db/connection");
 const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
+const jestSorted = require("jest-sorted");
 
 afterAll(() => {
-  if (db.end) return db.end();
+  return db.end();
 });
 beforeEach(() => {
   return seed(data);
 });
-describe.only("1. GET /api/topics", () => {
+describe("1. GET /api/topics", () => {
   test("status:200, responds with an array of topics objects", () => {
     return request(app)
       .get("/api/topics")
@@ -26,6 +27,139 @@ describe.only("1. GET /api/topics", () => {
             })
           );
         });
+      });
+  });
+});
+
+describe("4. GET /api/articles", () => {
+  test("status:200, responds with an array of articles objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        expect(articles.length).toBe(12);
+        articles.forEach((article) => {
+          expect(article).toEqual(
+            expect.objectContaining({
+              author: expect.any(String),
+              title: expect.any(String),
+              article_id: expect.any(Number),
+              topic: expect.any(String),
+              created_at: expect.any(String),
+              votes: expect.any(Number),
+              comment_count: expect.any(Number),
+            })
+          );
+        });
+      });
+  });
+  test("status:200, responds with an array of articles objects and the date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+});
+
+describe("5. GET /api/articles/:article_id", () => {
+  test("status:200, responds with an array of articles objects by article_id", () => {
+    const article_id = 3;
+    return request(app)
+      .get(`/api/articles/${article_id}`)
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toEqual(
+          expect.objectContaining({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: article_id,
+            body: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+          })
+        );
+      });
+  });
+  test("400: response with bad request if article_id is not a number", () => {
+    return request(app)
+      .get(`/api/articles/dog`)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad request");
+      });
+  });
+  test("404: response with not found for non existent article_id", () => {
+    return request(app)
+      .get(`/api/articles/1000`)
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+});
+
+describe("6. GET /api/articles/:article_id/comments", () => {
+  test("status:200, responds with an array of comments objects for the given article_id", () => {
+    const article_id = 3;
+    return request(app)
+      .get(`/api/articles/${article_id}/comments`)
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        expect(comments.length).toBe(2);
+        comments.forEach((comment) => {
+          expect(comment).toEqual(
+            expect.objectContaining({
+              comment_id: article_id,
+              votes: expect.any(Number),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+            })
+          );
+        });
+      });
+  });
+  test("status:200, responds with an array of comments objects and the date in descending order", () => {
+    const article_id = 3;
+    return request(app)
+      .get(`/api/articles/${article_id}/comments`)
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        expect(comments).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("404: response with not found for non existent article_id", () => {
+    return request(app)
+      .get(`/api/articles/1000/comments`)
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+  test("400: response with bad request if article_id is not a number", () => {
+    return request(app)
+      .get(`/api/articles/dog/comments`)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad request");
+      });
+  });
+  test("status:200, responds with an array of comments objects and the date in descending order", () => {
+    const article_id = 4;
+    return request(app)
+      .get(`/api/articles/${article_id}/comments`)
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        expect(comments).toEqual([]);
       });
   });
 });
--- models/news.js
+++ models/news.js
@@ -9,20 +9,42 @@ exports.selectTopics = () => {
 exports.selectArticles = () => {
   return db
     .query(
-      "SELECT articles.author, articles.title, articles.topic, articles.created_at, articles.votes, comments.article_id,CAST(COUNT(comments.article_id) AS int) as comment_count  FROM articles,comments GROUP BY articles.author, articles.title, articles.topic, articles.created_at, articles.votes, comments.article_id ORDER BY created_at DESC;"
+      "SELECT articles.* ,CAST(COUNT(comments.article_id) AS int) as comment_count FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id GROUP BY articles.article_id ORDER BY created_at DESC;"
     )
     .then(({ rows }) => {
       return rows;
     });
 };
 
-// exports.selectArticlesId = (articleId) => {
-//   return db
-//     .query(
-//       "SELECT articles.author, articles.title, comments.article_id , articles.body, articles.topic, articles.created_at, articles.votes, comments GROUP BY articles.author, articles.title, comments.article_id, articles.body, articles.topic, articles.created_at, articles.votes WHERE article_id = $1;",
-//       [articleId]
-//     )
-//     .then(({ rows }) => {
-//       console.log(rows);
-//     });
-// };
+exports.selectArticlesById = (articleId) => {
+  return db
+    .query("SELECT articles.* FROM articles  WHERE article_id = $1;", [
+      articleId,
+    ])
+    .then(({ rows }) => {
+      const article = rows[0];
+      if (!article) {
+        return Promise.reject({
+          status: 404,
+          msg: `Not Found`,
+        });
+      }
+      return article;
+    });
+};
+
+exports.selectComment = (articleId) => {
+  return db
+    .query(
+      "SELECT comments.votes, comments.created_at, comments.author, comments.body, comments.article_id as comment_id FROM comments WHERE comments.article_id = $1 GROUP BY comments.votes, comments.created_at, comments.author, comments.body, comments.article_id ORDER BY created_at DESC;",
+      [articleId]
+    )
+    .then(({ rows }) => {
+      const comments = rows;
+      return comments;
+    });
+};
+
+// if (comments.length === 0 && article) {
+//   return [];
+// }
